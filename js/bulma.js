4c81a838fede1b1beae8eb3c7a23ce3a4bac06be
Jck1997 <Jcke1997@gmail.com>
2018-07-25 18:06:19 +0100

    Updated the code to include new improved areas

--- js/bulma.js
+++ js/bulma.js
@@ -1,26 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
-  });
-  
+// Modal
+
+// Basildon
+let modalBasildonWhatsApp = document.getElementById('modal-basildon-Whatsapp');
+let basildonElements = document.getElementsByClassName('basildon-toggle-modal');
+for (var i = 0; i < basildonElements.length; i++) {
+  basildonElements[i].addEventListener('click', toggleClass);
+}
+function toggleClass() { modalBasildonWhatsApp.classList.toggle('is-active'); }
+
+// Noak Bridge
+let modalNoakbridgeWhatsApp = document.getElementById('modal-noakbridge-Whatsapp');
+let noakbridgeElements = document.getElementsByClassName('noakbridge-toggle-modal');
+for (var i = 0; i < noakbridgeElements.length; i++) {
+  noakbridgeElements[i].addEventListener('click', toggleClass);
+}
+function toggleClass() { modalNoakbridgeWhatsApp.classList.toggle('is-active'); }
+
+
+
