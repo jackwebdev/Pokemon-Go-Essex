b5e35640f4cee5158d7bddc45f5448f3beeff4e3
Jck1997 <Jcke1997@gmail.com>
2018-07-31 18:59:03 +0100

    New updates to the map.

--- js/map.js
+++ js/map.js
@@ -1,79 +1,182 @@
+// Maps
+var map;
+var place;
 
+// Layers to store markers
+var pokestops = L.layerGroup();
+var gyms = L.layerGroup();
+var exraids = L.layerGroup();
+var nests = L.layerGroup();
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFjazk3IiwiYSI6ImNqam80NHhiYjIydjcza20wd3AwajNzZGIifQ.oHWFzGB3jFhqwftPqWMHPw';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/jack97/cjjo4zrswhw3l2sm9okqdxh1g',
-    center: [0.41838735, 51.63418066], // starting position
-    minzoom: 3,
-    zoom: 16 // starting zoom
+// Arrays
+var markers = [];
+var pokestopMarkers = [];
+var gymMarkers = [];
+var nestMarkers = [];
+var pokedexEntryArr = [];
+var pokdexEntry;
 
-});
+// DB
+var idGyms = '1qi_xKyrQxbJ5ZyCjZdxT_1Pytk7C2Kjim6igfmc2sN0';
+var idPokestops = '1-RNWRuqAhZqgy7G2oLXlNoWIwdeRyyCpinS_lCQ8cFg'
+var idNests = '1NSKNguQesofI01JInw_OpNLL7Gysm4FEfW6mXXxKmFQ';
 
-// Add geolocate + control to the map.
-map.addControl(new mapboxgl.NavigationControl());
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
+var icons = {
+  'pokestop': L.icon({
+      iconUrl: 'images/icons/pokestop.svg',
+      iconSize:     [30,30]}),
+  'gym': L.icon({
+      iconUrl: 'images/icons/gym.svg',
+      iconSize:     [30,30]}),
+  'exraid': L.icon({
+      iconUrl: 'images/icons/exraid.svg',
+      iconSize:     [30,30]}),
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+
+  
+  map = L.map('map',{minZoom:12}).setView([51.63418066, 0.41838735], 16);
+  
+
+// Styles
+  //  Default
+  // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>| Developed by Jack'
+  // }).addTo(map);
+  //  Pokémon Go Style
+  L.tileLayer('https://api.mapbox.com/styles/v1/jack97/cjjo4zrswhw3l2sm9okqdxh1g/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFjazk3IiwiYSI6ImNqam80NHhiYjIydjcza20wd3AwajNzZGIifQ.oHWFzGB3jFhqwftPqWMHPw', 
+  { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> | Developed by Jack'}
+  ,{foo: 'bar'}).addTo(map);
+
+  // Locate your self
+L.control.locate({
+  strings: {
+      title: "Find your GPS location."
+  }
+}).addTo(map);
+
+  // Import Gyms
+  Tabletop.init({ 
+    key: idGyms,
+    callback: function(sheet, tabletop){ 
+      // Loop through the data
+      for (var i in sheet){
+        place = sheet[i];
+        // Test to check what data is coming through
+        // console.log(place);
+        // Check exraid and if yes we shall return the correct gymStatus
+        if(place.exraid === 'yes'){
+          // Add to markers array
+          gymMarkers.push(
+            // Gymstatus checks to display correct icon.
+          L.marker([place.latitude, place.longitude], {icon: icons['exraid']})
+            .addTo(exraids)
+            .bindPopup('<h3 class="marker-header">' + place.title + '</h3><p class="marker-text">' + place.description + '</p>')
+        );
+        } else {
+            // Add to markers array
+            gymMarkers.push(
+            // Gymstatus checks to display correct icon.
+            L.marker([place.latitude, place.longitude], {icon: icons['gym']})
+              .addTo(gyms)
+              .bindPopup('<h3 class="marker-header">' + place.title + '</h3><p class="marker-text">' + place.description + '</p>')
+            );
+        }
+      }
     },
-  		trackUserLocation: true
-
-}));
-
-// Popups
-map.on('click', function(e) {
-  var pokestops = map.queryRenderedFeatures(e.point, {
-    layers: ['pokestops'] // replace this with the name of the layer
+    simpleSheet: true 
   });
-
-  if (!pokestops.length) {
-    return;
-  }
-
-  	var pokestop = pokestops[0];
-
- 	var popupPokestops = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(pokestop.geometry.coordinates)
-    .setHTML('<h3><strong>' + pokestop.properties.title + '</strong></h3><p>' + pokestop.properties.description + '</p>')
-    .setLngLat(pokestop.geometry.coordinates)
-    .addTo(map);
-
-});
-
-map.on('click', function(e) {
-   var gyms = map.queryRenderedFeatures(e.point, {
-    layers: ['gyms'] // replace this with the name of the layer
+   // Import Pokestops
+   Tabletop.init({ 
+    key: idPokestops,
+    callback: function(sheet, tabletop){ 
+      // Loop through the data
+      for (var i in sheet){
+        place = sheet[i];
+        // Test to check what data is coming through
+        // console.log(place);
+        // Add to markers array
+        pokestopMarkers.push(
+          L.marker([place.latitude, place.longitude], {icon: icons['pokestop']})
+          .addTo(pokestops)
+          .bindPopup('<h3 class="marker-header">' + place.title + '</h3><p class="marker-text">' + place.description + '</p>')
+        );
+      }
+    },
+    simpleSheet: true 
   });
+     // Import Nests
+     Tabletop.init({ 
+      key: idNests,
+      callback: function(sheet, tabletop){ 
+        // Loop through the data
+        for (var i in sheet){
+          place = sheet[i];
+          // Test to check what data is coming through
+          // console.log(place);
+          // Get the image from pokedex entry and set it to the icon
+          iconNest = {
+          'nest': L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + Number(place.pokedexentry) + '.png',
+            iconSize:     [80,80]}),
+          }
+          // Add to markers array
+          nestMarkers.push(
+            L.marker([place.latitude, place.longitude], {icon: iconNest['nest']})
+            .addTo(nests)
+            .bindPopup('<h3 class="marker-header">' + place.title + '</h3><p class="marker-text">' + place.description + '</p>')
+          );
+        }
+      },
+      simpleSheet: true 
+    });
 
-  if (!gyms.length) {
-    return;
-  }
 
-  	var gym = gyms[0];
+// Push both gyms and pokestop to the main markers array
+setTimeout(() => {
+  markers =  gymMarkers.concat(pokestopMarkers);
+  // console.log(markers);
+  // map.on('moveend', renderMarkersInMapView);
+}, 2000);
 
-    var popupGyms = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(gym.geometry.coordinates)
-    .setHTML('<h3><strong>' + gym.properties.title + '</strong></h3><p>' + gym.properties.description + '</p>')
-    .setLngLat(gym.geometry.coordinates)
-    .addTo(map);
+// Add to laer
+map.addLayer(pokestops);
+map.addLayer(gyms);
+map.addLayer(exraids);
+map.addLayer(nests);
+// Layers for the marker controlUI
+var baseLayers = {};
+var overlays = {
+  "Pokestop": pokestops,  
+  "Gyms": gyms,
+  "Exraids": exraids,
+  "Nests": nests,
+};
+// Add Layer-Controller to toggle markers
+L.control.layers(baseLayers, overlays).addTo(map);
+
 
 });
 
-map.on('click', function(e) {
-  var exraids = map.queryRenderedFeatures(e.point, {
-   layers: ['exraids'] // replace this with the name of the layer
- });
-
- if (!exraids.length) {
-   return;
- }
-
-   var exraid = exraids[0];
-
-   var popupExraid = new mapboxgl.Popup({ offset: [0, -15] })
-   .setLngLat(exraid.geometry.coordinates)
-   .setHTML('<h3><strong>' + exraid.properties.title + '</strong></h3><p>' + exraid.properties.description + '</p>')
-   .setLngLat(exraid.geometry.coordinates)
-   .addTo(map);
-
-});
+// Functions
+  // Exraid Checker
+  function checkExraid() {
+      if(place.exraid === 'yes'){
+        gymlayerStatus = exraids;
+        gymStatus = 'exraid';
+      } 
+    }
+    // Hide markers not in view
+  //   function renderMarkersInMapView() {
+  //     var mapBounds = map.getBounds();
+  //     for (var i = markers.length -1; i >= 0; i--) {
+  //         var m = markers[i];
+  //         var shouldBeVisible = mapBounds.contains(m.getLatLng());
+  //         if (m._icon && !shouldBeVisible) {
+  //             map.removeLayer(m);
+  //         } else if (!m._icon && shouldBeVisible) {
+  //             map.addLayer(m);
+  //         }
+  //     }
+  // }
